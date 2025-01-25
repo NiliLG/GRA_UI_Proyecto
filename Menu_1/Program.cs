20e26a06fc01cb975f65c893edde8cef21b8c5f6
NiliLG <147437701+NiliLG@users.noreply.github.com>
2025-01-24 22:16:58 -0600

    Versión_02: Otro cambio

---
+++ Menu_1/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = 25;
+        int y = 115;
+        int c = 0;
+        for (int i = 0; i < 115; i++)
+        {
+            Console.SetCursorPosition(y, x);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("*");
+            c++;
+            if (c == 7)
+            {
+                x--;
+                c= 0;
+                for (int a = 0; a < 5; a++)
+                {
+                    Console.SetCursorPosition(y, x);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("*");
+                    y--;
+                }
+            }
+          
+
+        }
+        Console.ReadKey();
+    }
+}
