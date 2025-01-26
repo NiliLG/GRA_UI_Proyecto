26bf86fa23a65e0666fe5b8ea6c497ff4be7c946
NiliLG <147437701+NiliLG@users.noreply.github.com>
2025-01-26 13:35:14 -0600

    Versión_04: Menú

--- Menu_1/Program.cs
+++ Menu_1/Program.cs
@@ -5,10 +5,41 @@ class Program
 {
     static void Main()
     {
-        // Barras
+        int opcion;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.SetCursorPosition(50, 2);
+        Console.WriteLine("Menú");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("1. Gráfica de barras con asteriscos");
+        Console.WriteLine("2. Espiral de asteriscos");
+        Console.WriteLine("2. Salir");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("\nSelecciona una opción: ");
+        opcion = int.Parse(Console.ReadLine());
+        switch (opcion)
+        {
+            case 1:
+                DibujarGrafica();
+                break;
+            case 2:
+                DibujarEspiral();
+                break;
+            case 3:
+                Console.WriteLine("Saliendo...");
+                break;
+            default:
+                Console.WriteLine("Opción no válida. Intenta de nuevo.");
+                break;
+        }
+        Console.ReadKey();
+    }
+    static void DibujarGrafica()
+    {
+        Console.Clear();
+        //Tamaño de barras
         int altura = 13;
         int ancho = 6;
-        // Posición inicial 
+        //Posición inicial 
         int x = 22;
         int y = 119;
 
@@ -17,14 +48,14 @@ class Program
         Console.SetCursorPosition(50, 5);
         Console.WriteLine("Dibujar gráfica");
 
-        // Tamaño de pantalla
+        //Tamaño de pantalla
         int consoleWidth = Console.WindowWidth;
         int consoleHeight = Console.WindowHeight;
 
-        // Gráfica
+        //Gráfica
         while (y > 0)
         {
-            // Línea horizontal hacia la izquierda
+            //<-
             Console.ForegroundColor = ConsoleColor.Cyan;
             for (int i = 0; i < ancho; i++)
             {
@@ -35,14 +66,11 @@ class Program
                     Thread.Sleep(50);
                 }
             }
-
-
-
             y -= ancho; // Retroceder en el eje horizontal
 
-            // Línea vertical hacia arriba
+            //Arriba
             Console.ForegroundColor = ConsoleColor.Blue;
-            for (int i = 0; i < 14; i++) // Cambiado a 14 asteriscos azules
+            for (int i = 0; i < 14; i++)
             {
                 if (y >= 0 && x - i >= 0)
                 {
@@ -52,24 +80,22 @@ class Program
                 }
             }
 
-            // Línea horizontal hacia la izquierda
+            //<-
             Console.ForegroundColor = ConsoleColor.Cyan;
             for (int i = 0; i < ancho; i++)
             {
                 if (y - i >= 0 && y - i < consoleWidth && x - altura >= 0)
                 {
-                    Console.SetCursorPosition(y - i-1, x - altura);
+                    Console.SetCursorPosition(y - i - 1, x - altura);
                     Console.WriteLine("*");
-                    Thread.Sleep(50); // Pausa de 50 ms entre cada asterisco
+                    Thread.Sleep(50);
                 }
             }
-
-            
-
             y -= ancho; // Retroceder en el eje horizontal
-            // Línea vertical hacia abajo
+
+            //Abajo
             Console.ForegroundColor = ConsoleColor.Blue;
-            for (int i = 0; i < 14; i++) // Cambiado a 14 asteriscos azules
+            for (int i = 0; i < 14; i++)
             {
                 if (y >= 0 && x - altura + i < consoleHeight)
                 {
@@ -79,13 +105,14 @@ class Program
                 }
             }
 
-            
         }
 
         Console.ForegroundColor = ConsoleColor.Green;
         Console.SetCursorPosition(40, 25);
         Console.WriteLine("Listo!!! Presiona una tecla para continuar");
+    }
+    static void DibujarEspiral()
+    { 
 
-        Console.ReadKey();
     }
 }
